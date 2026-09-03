49de43c786d02fc9e7dd374e0309814fd05eb475
Aeywoo <Aeywoo@proton.me>
2026-09-04 03:44:20 +0930

    PHPCS fixes
    Minor fixes.

--- tests/phpunit/EmbedService/YouTube/YouTubePlaylistTest.php
+++ tests/phpunit/EmbedService/YouTube/YouTubePlaylistTest.php
@@ -91,6 +91,7 @@ class YouTubePlaylistTest extends MediaWikiIntegrationTestCase {
 	public function testUrl() {
 		$service = new YouTubePlaylist( $this->validUrlId );
 
+		// phpcs:ignore Generic.Files.LineLength.TooLong
 		$this->assertStringContainsString( 'https://www.youtube-nocookie.com/embed/videoseries?list=', $service->getUrl() );
 	}
 }
