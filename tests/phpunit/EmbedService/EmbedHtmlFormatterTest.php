34d53387815ff84b3c4b0100913ab3e3546b0f3e
Aeywoo <Aeywoo@proton.me>
2026-09-04 03:49:38 +0930

    Update EmbedHtmlFormatterTest.php
    Read previous commit.

--- tests/phpunit/EmbedService/EmbedHtmlFormatterTest.php
+++ tests/phpunit/EmbedService/EmbedHtmlFormatterTest.php
@@ -44,7 +44,7 @@ class EmbedHtmlFormatterTest extends MediaWikiIntegrationTestCase {
 		$service = EmbedServiceFactory::newFromName( 'archiveorg', 'foo' );
 
 		// phpcs:ignore Generic.Files.LineLength.TooLong
-		$this->assertStringContainsString( 'data-mw-iframeconfig="{&quot;src&quot;:&quot;//archive.org/embed/foo&quot;}"', EmbedHtmlFormatter::toHtml( $service ) );
+		$this->assertStringContainsString( 'data-mw-iframeconfig="{&quot;src&quot;:&quot;https://archive.org/embed/foo&quot;}"', EmbedHtmlFormatter::toHtml( $service ) );
 		$this->assertStringNotContainsString( '<iframe', EmbedHtmlFormatter::toHtml( $service ) );
 	}
 
